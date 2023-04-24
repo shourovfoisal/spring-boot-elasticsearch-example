7cf2b710189f19d091dfbe30623a243d4cdfdb34
Shourov Foisal <shourovfoisal@gmail.com>
2023-04-24 22:10:07 +0600

    Added controller routes to the main app file

--- src/main/java/com/stech/SpringBootElasticsearchExampleApplication.java
+++ src/main/java/com/stech/SpringBootElasticsearchExampleApplication.java
@@ -1,13 +1,40 @@
 package com.stech;
 
+import com.stech.model.Employee;
+import com.stech.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SpringBootElasticsearchExampleApplication {
+
+	@Autowired
+	private EmployeeRepository repository;
+
+	@PostMapping("/saveEmployee")
+	public int saveEmployee(@RequestBody List<Employee> employees) {
+		System.out.println(employees.toString());
+		repository.saveAll(employees);
+		return employees.size();
+	}
+
+	@GetMapping("/findEverything")
+	public Iterable<Employee> findAllEmployees() {
+		return repository.findAll();
+	}
+
+	@GetMapping("/findByFName/{firstName}")
+	public List<Employee> findByFirstName(@PathVariable String firstName) {
+		return repository.findByFirstname(firstName);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootElasticsearchExampleApplication.class, args);
 	}
-
 }
+
